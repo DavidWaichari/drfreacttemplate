e859bfd12c15f3430bef11ecf0f568acabf04dc3
David Waichari <dwaichari@gmail.com>
2019-03-08 15:30:08 +0300

    add login form capability

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -1,28 +1,93 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+class Login extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: ""
+  };
+  handleOnchange = () => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
 
-class Login extends Component{
-    state = {
-        username:'',
-        email:'',
-        password:''
-    }
-    render(){
-        return(
-            
-                    <form>
-                            <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                            </div>
-                            <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                            </div>
-                            <button type="submit" className="btn btn-primary">Login</button>
-                    </form>
-                
-        )
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    const logindetails = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios
+      .post("/rest-auth/login/", logindetails)
+      .then(response => {
+        console.log("login sucessful");
+      })
+      .catch(error => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            aria-describedby="emailHelp"
+            placeholder="Enter username"
+            value={this.state.username}
+            onChange={this.handleOnchange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handleOnchange}
+          />
+          <small id="email" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleOnchange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    );
+  }
 }
 export default Login;
