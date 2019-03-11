fe0a3d28f04b336aedd93df96e1864fa9ac97044
David Waichari <dwaichari@gmail.com>
2019-03-11 15:23:34 +0300

    add router to login and register

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Login from "./authentication/Login";
 import Register from "./authentication/Register";
+import Navbar from "./layout/Navbar";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from app Component</h3>
-        <Login />
-        <Register />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </div>
+      </Router>
     );
   }
 }
